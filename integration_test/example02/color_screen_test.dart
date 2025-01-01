121b5e883202261fa5194d5d8f9b50b1824f066d
samin <84340577+HGSChandeepa@users.noreply.github.com>
2025-01-01 23:37:15 +0530

    Update color_screen_test.dart

--- integration_test/example02/color_screen_test.dart
+++ integration_test/example02/color_screen_test.dart
@@ -1,4 +1,3 @@
-// integration_test/app_test.dart
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
 import 'package:flutter/material.dart';
